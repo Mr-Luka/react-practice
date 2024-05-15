54a7a1fe443660838a7b213bc3c3c2fb68fa5fee
Luka <luka.luka.luka93@gmail.com>
2024-05-15 16:37:57 -0700

    adding profiles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import suggestedFriends from "./index.js";
 {/* writting a comment */}
 const divStyle = { backgroundColor: "blue" };
 divStyle.backgroundColor = "red";
@@ -13,6 +14,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <div>
+        {suggestedFriends.map((friend)=>{
+          <div className="card">
+            <img className="picture" src={friend.picture} alt="user profile"/>
+          </div>
+        })}
           <h1 id="page-tittle">{firstName} {lastName}'s Page</h1>
           <h2>I crush skillz. All day, everyday.</h2>
         </div>
