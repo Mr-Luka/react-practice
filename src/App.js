85a3cb6b69083aa8edbfbed6cc47f05e5c4a444c
Luka <luka.luka.luka93@gmail.com>
2024-05-15 16:41:49 -0700

    no logo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import suggestedFriends from "./index.js";
 {/* writting a comment */}
@@ -7,23 +7,18 @@ divStyle.backgroundColor = "red";
 
 
 function App() {
-  const firstName = "Luka";
-  const lastName = "Pav";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
         <div>
         {suggestedFriends.map((friend)=>{
           <div className="card">
             <img className="picture" src={friend.picture} alt="user profile"/>
           </div>
         })}
-          <h1 id="page-tittle">{firstName} {lastName}'s Page</h1>
-          <h2>I crush skillz. All day, everyday.</h2>
+
         </div>
-      </header>
-    </div>
+
   );
 }
 
