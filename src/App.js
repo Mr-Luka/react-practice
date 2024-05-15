6103949f2b73e9440262ceb62e04c49bfefe708a
Luka <luka.luka.luka93@gmail.com>
2024-05-15 15:51:33 -0700

    crushing the skills

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import './App.css';
 {/* writting a comment */}
 const divStyle = { backgroundColor: "blue" };
 divStyle.backgroundColor = "red";
+
+const firstName = "Skill";
+const lastName = "Crusher";
+const renderVariables = <h1>Hey {firstName + " " + lastName}!</h1>;
+// this element will render a p element with 8 as the text
+const evaluateAnyExpressions = <p>{1 + 2 + 5}</p>;
+function getTime() {
+  const now = new Date();
+  return now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+}
 function App() {
   return (
     <div className="App">
