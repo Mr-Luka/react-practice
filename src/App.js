aa28aa3231fc6ce6c640749b692f3721e761f0da
Luka <luka.luka.luka93@gmail.com>
2024-05-15 16:07:36 -0700

    first and last name with better understanding

--- src/App.js
+++ src/App.js
@@ -4,22 +4,16 @@ import './App.css';
 const divStyle = { backgroundColor: "blue" };
 divStyle.backgroundColor = "red";
 
-const firstName = "Skill";
-const lastName = "Crusher";
-const renderVariables = <h1>Hey {firstName + " " + lastName}!</h1>;
-// this element will render a p element with 8 as the text
-const evaluateAnyExpressions = <p>{1 + 2 + 5}</p>;
-function getTime() {
-  const now = new Date();
-  return now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
-}
+
 function App() {
+  const firstName = "Luka";
+  const lastName = "Pav";
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <div>
-          <h1 id="page-tittle">Luka's Page</h1>
+          <h1 id="page-tittle">{firstName} {lastName}'s Page</h1>
           <h2>I crush skillz. All day, everyday.</h2>
         </div>
       </header>
