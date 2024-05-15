90e62d5d8ab830a51a7ccbc13f47787c3092b240
Luka <luka.luka.luka93@gmail.com>
2024-05-15 15:41:12 -0700

    practice

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          <h1 id="page-tittle">Luka's Page</h1>
+          <h2>I crush skillz. All day, everyday.</h2>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
