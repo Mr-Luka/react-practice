7c71dba86bacf822ea25b45bab80ecc911514d07
Luka <luka.luka.luka93@gmail.com>
2024-05-15 17:11:49 -0700

    added

--- src/App.js
+++ src/App.js
@@ -15,7 +15,14 @@ const lastName= "Pav";
       <br />
       <main>
         <h2>People You May Know {firstName} {lastName}</h2>
-    
+        <div className="card">
+                <button className="primary">Add Friend</button>
+                <button>Remove</button>
+                <div className="card-text">
+                  <h5 className="username">{firstName}</h5>
+                  <span className="mutual-friends">{lastName}</span>
+                </div>
+        </div>    
         {suggestedFriends.map((friend)=>{
           <div className="card">
             <img className="picture" src={friend.picture} alt="user profile"/>
@@ -23,8 +30,6 @@ const lastName= "Pav";
               <h5 className="username">{friend.name}</h5>
               <span className="mutual-friends">{friend.mutualFriendCount}</span>
               <div className="card-actions">
-                <button className="primary">Add Friend</button>
-                <button>Remove</button>
               </div>
             </div>
           </div>
